5756cbb9712aa51e91a12ed4f666a5277c40eaf8
JacobBDon <jacob.b.don@gmail.com>
2026-04-23 18:39:37 -0500

    Update StreamlitApp_final.py

--- StreamlitApp_final.py
+++ StreamlitApp_final.py
@@ -50,8 +50,7 @@ def welcome_dialog():
 	\n\nTo create a list of trips according to your selected criteria, use the Trip Selector.
 	\n\nTo view trip itineraries, use the Itinerary Selector.
 	\n\nFor an easy way to create your trip rankings (1-10 minutes), use the Easy Ranker.
-	\n\nThis tool represents a fraction of the rich information provided by the Official Random Walks website.
-	\nFor trip leaders, as well as detailed tips and tricks for each itinerary, please visit https://www.randomwalksbooth.org/
+	\n\nThis tool represents a fraction of the rich information provided by the Official Random Walks website.\nFor trip leaders, as well as detailed tips and tricks for each itinerary, please visit https://www.randomwalksbooth.org/
 	\n\nThis tool collects no data, and sessions are cleared upon refresh.""")
     if st.button("Let's go!"):
         st.session_state["welcomed"] = True
